135e34b2b9f749cd4ef75c0df74cc1cfd5b7a96e
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-03-28 12:58:08 -0500

    Project.js now contains github and deployed links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import GitHubLogo from "../images/Github-Logo.jpg";
 
 class Project extends Component {
+  state = {
+    git: this.props.git,
+    url: this.props.url,
+    image: this.props.image,
+  }
+
   render () {
+    const style = {
+      images: {
+        maxWidth:"250px",
+        maxHeight: "200px",
+        marginRight: "5px",
+      },
+      git: {
+        height: "50px",
+        width: "50px",
+      }
+    }
     return (
       <div>
-
+        <a href={this.state.url} target="_blank" rel="noopener noreferrer"><img style={style.images} src={this.state.image} alt="Deployed"/></a>
+        <a href={this.state.git} target="_blank" rel="noopener noreferrer"><img style={style.git} src={GitHubLogo} alt="Github Logo"/></a>
       </div>
     );
   }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Project from "../components/Project";
+import GooderReads from "../images/gooder_reads.jpg";
 
 class Portfolio extends Component {
   render() {
@@ -7,7 +8,7 @@ class Portfolio extends Component {
       <div>
         <h3>Portfolio</h3>
         <hr />
-
+        <Project url="https://goodest-reads.herokuapp.com/" git="https://github.com/shayneofficer/GooderReads" image={GooderReads}/>
       </div>
     );
   }
