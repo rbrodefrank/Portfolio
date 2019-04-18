d3e5eda75c0a745b89ca13b53942e4da061518d7
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-04-18 15:48:16 -0500

    edited Navbar to have tabs that are actived

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
+// import Contact from './pages/Contact';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <NavBar />
+      <div>
+        <NavBar />
+        <Router>
           <div className="container">
             <Route exact path="/" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/projects" component={Portfolio} />
             {/* <Route exact path="/contact" component={Contact} /> */}
           </div>
-        </div>
-      </Router>
+        </Router>
+      </div>
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,34 @@ import React, { Component } from "react";
 import GitHubLogo from "../images/Github-Logo.jpg";
 import LinkedInLogo from "../images/linkedin.jpg";
 
-class Navbar extends Component {
+class Navigation extends Component {
+  componentDidMount = () => {
+    var active = document.getElementById(window.location.pathname.slice(1));
+    var removal = document.getElementsByClassName("active")[0];
+    if(removal) removal.classList.remove("active");
+    if(active) active.classList.add("active");
+  }
+
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <span className="mr-auto">
-          <a className="navbar-brand" id="Main" href="/">
-            Ryan BrodeFrank
-          </a>
-          <a className="navbar-brand" id="Portfolio" href="/portfolio">Portfolio</a>
-        </span>
-        <span>
-          <a className="navbar-brand" href="https://github.com/rbrodefrank" target="_blank" rel="noopener noreferrer" ><img style={{ height: "30px", width: "30px" }} src={GitHubLogo} alt="Github Link" /></a>
-          <a className="navbar-brand" href="https://www.linkedin.com/in/ryan-brodefrank-7933288b/" target="_blank" rel="noopener noreferrer" ><img style={{ height: "30px", width: "30px" }} src={LinkedInLogo} alt="Linkedin Link" /></a>
-        </span>
+        <a className="navbar-brand" id="main" href="/">Ryan BrodeFrank</a>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link active" id="projects" href="/projects">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" id="about" href="/about">About</a>
+          </li>
+        </ul>
+        <ul className="navbar-nav">
+          <a className="navbar-brand" href="https://github.com/rbrodefrank" target="_blank" rel="noopener noreferrer" ><img style={{ height: "40px", width: "40px" }} src={GitHubLogo} alt="Github Link" /> GitHub</a>
+          <a className="navbar-brand" href="https://www.linkedin.com/in/ryan-brodefrank-7933288b/" target="_blank" rel="noopener noreferrer" ><img style={{ height: "40px", width: "40px" }} src={LinkedInLogo} alt="Linkedin Link" /> LinkedIn</a>
+        </ul>
         {/* <a className="navbar-brand" id="Contact" href="/contact">Contact</a> */}
       </nav>
     );
   }
 }
 
-export default Navbar;
+export default Navigation;
