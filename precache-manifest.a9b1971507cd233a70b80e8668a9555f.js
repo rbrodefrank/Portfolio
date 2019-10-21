d73061943cb8661f2994d4dc68e0d8495d140bec
Ryan BrodeFrank <ryanafrank@gmail.com>
2019-10-21 12:41:51 -0500

    Updates

---
+++ precache-manifest.a9b1971507cd233a70b80e8668a9555f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c26d50fe0d398a3c1784440a76c28fc4",
+    "url": "/Portfolio/static/media/cross_news.c26d50fe.jpg"
+  },
+  {
+    "revision": "12084bfb6807e37d7bb3",
+    "url": "/Portfolio/static/css/main.b1e9da66.chunk.css"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "f93d781dc8c82ff6d0c1",
+    "url": "/Portfolio/static/js/2.586bbc28.chunk.js"
+  },
+  {
+    "revision": "1e24609c27ab63d86104154050d80aa2",
+    "url": "/Portfolio/static/media/background_developer.1e24609c.jpg"
+  },
+  {
+    "revision": "b8527d1f215cd35d3244d8e7078086bb",
+    "url": "/Portfolio/static/media/profile_pic.b8527d1f.jpg"
+  },
+  {
+    "revision": "0ed3f7b621979538efb0de2629522d9d",
+    "url": "/Portfolio/static/media/pos_system.0ed3f7b6.jpg"
+  },
+  {
+    "revision": "bf69c4fd855f39f6d574d55e4a544735",
+    "url": "/Portfolio/static/media/gooder_reads.bf69c4fd.jpg"
+  },
+  {
+    "revision": "12084bfb6807e37d7bb3",
+    "url": "/Portfolio/static/js/main.9e572d45.chunk.js"
+  },
+  {
+    "revision": "3d32cce2de05b150bea48fcfe4d977c1",
+    "url": "/Portfolio/static/media/giftastic.3d32cce2.jpg"
+  },
+  {
+    "revision": "1ade9eac7fca4bf0c620846007b3c0b1",
+    "url": "/Portfolio/static/media/bamazon.1ade9eac.jpg"
+  },
+  {
+    "revision": "4bb6f3875ea39fd03c79c9cb57f1fc72",
+    "url": "/Portfolio/static/media/trivia_game.4bb6f387.jpg"
+  },
+  {
+    "revision": "fc83c65adc97bba27459c265d6773837",
+    "url": "/Portfolio/static/media/word_guess.fc83c65a.jpg"
+  },
+  {
+    "revision": "f8f787322ecd9772c952e09922ac92c5",
+    "url": "/Portfolio/static/media/starwars.f8f78732.jpg"
+  },
+  {
+    "revision": "10e1ee6c865f63ca12477b128a40f1d3",
+    "url": "/Portfolio/static/media/background_forest.10e1ee6c.jpg"
+  },
+  {
+    "revision": "f9c13e25b6449fe3b80a45616a9cc224",
+    "url": "/Portfolio/index.html"
+  }
+];
